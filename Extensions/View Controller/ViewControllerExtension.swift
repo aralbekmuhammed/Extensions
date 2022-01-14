dcf4b7bb316221b0c4063a3d51fa1282dd1257ea
Aralbek Muhammed <aralbekmuhammed.2007@gmail.com>
2022-01-14 19:52:06 +0600

    -

--- Extensions/View Controller/ViewControllerExtension.swift
+++ Extensions/View Controller/ViewControllerExtension.swift
@@ -3,6 +3,7 @@ import StoreKit
 import SafariServices
 import AVFoundation
 import AVKit
+import Then
 //import JGProgressHUD
 //let spinner = JGProgressHUD(style: .light)
 struct AlertAction{
@@ -12,22 +13,18 @@ struct AlertAction{
 }
 
 func takeScreenshot(_ shouldSave: Bool = true) -> UIImage? {
-   var screenshotImage :UIImage?
-   let layer = UIApplication.shared.keyWindow!.layer
-   let scale = UIScreen.main.scale
-   UIGraphicsBeginImageContextWithOptions(layer.frame.size, false, scale);
-   guard let context = UIGraphicsGetCurrentContext() else {return nil}
-   layer.render(in:context)
-   screenshotImage = UIGraphicsGetImageFromCurrentImageContext()
-   UIGraphicsEndImageContext()
-   return screenshotImage
+    var screenshotImage :UIImage?
+    let layer = UIApplication.shared.keyWindow!.layer
+    let scale = UIScreen.main.scale
+    UIGraphicsBeginImageContextWithOptions(layer.frame.size, false, scale);
+    guard let context = UIGraphicsGetCurrentContext() else {return nil}
+    layer.render(in:context)
+    screenshotImage = UIGraphicsGetImageFromCurrentImageContext()
+    UIGraphicsEndImageContext()
+    return screenshotImage
 }
 extension UIViewController {
-    var navBarHeight: CGFloat{
-        UIApplication.shared.statusBarFrame.size.height +
-        (self.navigationController?.navigationBar.frame.height ?? 0.0)
-
-    }
+    //MARK: - Navigation Bar
     func setNavBar(cornerRadius: CGFloat,
                    color: UIColor){
         self.navigationController?.navigationBar.setBackgroundImage(UIImage(), for: .default)
@@ -42,21 +39,30 @@ extension UIViewController {
         }
         self.view.addSubview(navBarView)
     }
+    
+    var navBarHeight: CGFloat{
+        UIApplication.shared.statusBarFrame.size.height +
+        (self.navigationController?.navigationBar.frame.height ?? 0.0)
+        
+    }
+    
     func presentRateAlert(){
         guard let window = view.window,
-        let scene = window.windowScene else{return}
+              let scene = window.windowScene else{return}
         if #available(iOS 14.0, *) {
             SKStoreReviewController.requestReview(in: scene)
         } else {
             SKStoreReviewController.requestReview()
         }
     }
+    
     func openWebsite(_ string: String){
         guard let url = URL(string: string) else{return}
         present(SFSafariViewController(url: url),
-                animated: true) 
+                animated: true)
         
     }
+    
     func openVideo(from url: URL){
         let player = AVPlayer(url: url)
         let playerController = AVPlayerViewController().then {
@@ -66,17 +72,25 @@ extension UIViewController {
             player.play()
         }
     }
+    
     func callToPhoneNumber(_ phoneNumber: String){
         guard let number = URL(string: "tel://" + phoneNumber) else {print("looks like it's something wrong with phone number"); return }
         UIApplication.shared.open(number)
     }
-    func showAlert(withTitle: String?, message: String?, style: UIAlertController.Style = .alert, actions: [AlertAction] = [AlertAction(text: "Ок", type: .default)]){
+    
+    func showAlert(withTitle: String?,
+                   message: String?,
+                   alpha: CGFloat = 1,
+                   style: UIAlertController.Style = .alert,
+                   actions: [AlertAction] = [AlertAction(text: "Ок", type: .default)]){
         let alert = UIAlertController(title: withTitle, message: message, preferredStyle: style)
+        alert.view.alpha = alpha
         actions.forEach { action in
             alert.addAction(UIAlertAction(title: action.text, style: action.type, handler: action.action))
         }
         present(alert, animated: true, completion: nil)
     }
+    
     func presentViewController(viewController: UIViewController,
                                presentationStyle: UIModalPresentationStyle,
                                transitionStyle: UIModalTransitionStyle,
